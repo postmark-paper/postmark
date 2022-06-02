6fc684b918182bbbfe7e406b41d8c5152e73fa42
Tony <tony.so@bunjang.co.kr>
2022-06-02 16:29:04 +0900

    init-commit

---
+++ src/main/java/com/example/postmark/PostmarkApplication.java
@@ -0,0 +1,13 @@
+package com.example.postmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostmarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostmarkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/postmark/PostmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostmarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
